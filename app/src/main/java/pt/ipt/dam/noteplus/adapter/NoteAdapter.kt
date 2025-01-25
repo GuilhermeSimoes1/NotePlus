f10ad47d07f2c76b2a48081c7b84d18a495000e0
Jescarameia <estt2287@ipt.pt>
2025-01-25 19:22:57 +0000

    edicao notas
    upgrade para edição das notas

--- app/src/main/java/pt/ipt/dam/noteplus/adapter/NoteAdapter.kt
+++ app/src/main/java/pt/ipt/dam/noteplus/adapter/NoteAdapter.kt
@@ -1,5 +1,4 @@
 package pt.ipt.dam.noteplus.adapter
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,10 +7,10 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import pt.ipt.dam.noteplus.R
 import pt.ipt.dam.noteplus.model.Note
-
-class NoteAdapter(private var notes: List<Note>) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
-
-    // Defina um objeto DiffUtil.Callback para otimizar as mudanças
+class NoteAdapter(
+    private var notes: List<Note>,
+    private val onNoteClick: (Note) -> Unit
+) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
     class NoteDiffCallback(
         private val oldList: List<Note>,
         private val newList: List<Note>
@@ -19,40 +18,33 @@ class NoteAdapter(private var notes: List<Note>) : RecyclerView.Adapter<NoteAdap
         override fun getOldListSize() = oldList.size
         override fun getNewListSize() = newList.size
         override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldList[oldItemPosition].id == newList[newItemPosition].id // Assumindo que Note tenha um id único
+            return oldList[oldItemPosition].id == newList[newItemPosition].id
         }
-
         override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
             return oldList[oldItemPosition] == newList[newItemPosition]
         }
     }
-
-    // Função para atualizar a lista de notas com a comparação do DiffUtil
     fun updateNotes(newNotes: List<Note>) {
         val diffCallback = NoteDiffCallback(notes, newNotes)
         val diffResult = DiffUtil.calculateDiff(diffCallback)
         notes = newNotes
         diffResult.dispatchUpdatesTo(this)
     }
-
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val noteTitle: TextView = itemView.findViewById(R.id.noteTitle)
         val noteDesc: TextView = itemView.findViewById(R.id.noteDesc)
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.note, parent, false)
         return NoteViewHolder(view)
     }
-
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         val note = notes[position]
         holder.noteTitle.text = note.title
         holder.noteDesc.text = note.description
+        holder.itemView.setOnClickListener {
+            onNoteClick(note)
+        }
     }
-
     override fun getItemCount() = notes.size
-
-
-
 }
