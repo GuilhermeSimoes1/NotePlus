fc099485df9a9246ac958828c3135952e0fdc081
Jescarameia <estt2287@ipt.pt>
2025-01-25 00:36:19 +0000

    Lógica das notas
    Lógica para pesquisar as notas
    Lógica para atualizar a pesquisa em tempo real

---
+++ app/src/main/java/pt/ipt/dam/noteplus/adapter/NoteAdapter.kt
@@ -0,0 +1,30 @@
+package pt.ipt.dam.noteplus.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import pt.ipt.dam.noteplus.R
+import pt.ipt.dam.noteplus.model.Note
+
+class NoteAdapter(private val notes: List<Note>) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val noteTitle: TextView = itemView.findViewById(R.id.noteTitle)
+        val noteDesc: TextView = itemView.findViewById(R.id.noteDesc)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note, parent, false)
+        return NoteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.noteTitle.text = note.title
+        holder.noteDesc.text = note.description
+    }
+
+    override fun getItemCount() = notes.size
+}
--- app/src/main/java/pt/ipt/dam/noteplus/fragments/HomeFragment.kt
+++ app/src/main/java/pt/ipt/dam/noteplus/fragments/HomeFragment.kt
@@ -1,12 +1,18 @@
 package pt.ipt.dam.noteplus.fragments
-
+import pt.ipt.dam.noteplus.adapter.NoteAdapter
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import pt.ipt.dam.noteplus.R
 
 @Suppress("DEPRECATION")
-class HomeFragment : Fragment(R.layout.home_fragment) {
+class HomeFragment() : Fragment(R.layout.home_fragment), Parcelable {
+
+    constructor(parcel: Parcel) : this() {
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,16 +24,47 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_home, menu) // Infle o menu
+        val searchItem = menu.findItem(R.id.searchMenu)
+        val searchView = searchItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // Lógica para pesquisar as notas
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // Lógica para atualizar a pesquisa em tempo real
+                return false
+            }
+        })
         super.onCreateOptionsMenu(menu, inflater)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.searchMenu -> {
-                // Ação para buscar notas
+                // Ação para encontrar as notas
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<HomeFragment> {
+        override fun createFromParcel(parcel: Parcel): HomeFragment {
+            return HomeFragment(parcel)
+        }
+
+        override fun newArray(size: Int): Array<HomeFragment?> {
+            return arrayOfNulls(size)
         }
     }
 }
